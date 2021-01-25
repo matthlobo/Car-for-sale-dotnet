8fd6bc0623b865fe0f3d9802ead36d66fd3089a1
Matheus Lobo <matheusbrbg@gmail.com>
2021-01-25 13:29:13 -0300

    Subindo primeira versão do CRUD da API

---
+++ CarForSale/Model/Fornecedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarForSale.Model
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "O Campo {0} é obrigatório!")]
+        [StringLength(30, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+        public string Codigo { get; set; }
+    }
+}
